2cbefdbc8c0ec8ce2cfbda97a7aad2870e9ab11c
yunhalee05 <yunhalee0506@naver.com>
2022-05-03 09:50:33 +0900

    Feat: 예외 케이스 추가

---
+++ app/src/main/java/nextstep/jwp/exception/HeaderNotFoundException.java
@@ -0,0 +1,16 @@
+package nextstep.jwp.exception;
+
+public class HeaderNotFoundException extends RuntimeException{
+
+    public HeaderNotFoundException(String message) {
+        super(message);
+    }
+
+    public HeaderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public HeaderNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
