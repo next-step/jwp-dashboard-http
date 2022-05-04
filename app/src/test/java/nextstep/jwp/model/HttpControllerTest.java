cdf5bc7b9e2bd311f301cd9b5d14e9de947bc2e4
yunhalee05 <yunhalee0506@naver.com>
2022-05-04 22:19:16 +0900

    Feat: Login 테스트 구현

---
+++ app/src/test/java/nextstep/jwp/model/HttpControllerTest.java
@@ -0,0 +1,55 @@
+package nextstep.jwp.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HttpControllerTest {
+
+    private HttpController httpController = new HttpController();
+
+
+    @Test
+    public void get_method_response() throws IOException {
+        String[] headers = {"Host: localhost:8080 ", "Connection: keep-alive "};
+        HttpRequest request = new HttpRequest("GET static/index.html HTTP/1.1 ", headers, "");
+        HttpResponse response = httpController.getResponse(request);
+        String expected = "HTTP/1.1 200 OK \r\n"
+            + "Content-Type: text/html;charset=utf-8 \r\n"
+            + "Content-Length: 12 \r\n"
+            + "\r\n"
+            + "Hello world!";
+        assertThat(response.toString()).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void post_method_login_response() {
+        String[] headers = {"Host: localhost:8080 ", "Connection: keep-alive "};
+        HttpRequest request = new HttpRequest("POST /login?account=gugu&password=password HTTP/1.1 ", headers, "");
+        HttpResponse response = httpController.getResponse(request);
+        String expected = "HTTP/1.1 302 Found \r\n"
+            + "Location: static/index.html \r\n"
+            + "\r\n"
+            + "";
+        assertThat(response.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void post_method_login_with_invalid_password_response() {
+        String[] headers = {"Host: localhost:8080 ", "Connection: keep-alive "};
+        HttpRequest request = new HttpRequest("POST /login?account=gugu&password=invalidPassword HTTP/1.1 ", headers, "");
+        HttpResponse response = httpController.getResponse(request);
+        String expected = "HTTP/1.1 401 Unauthorized \r\n"
+            + "Location: static/401.html \r\n"
+            + "\r\n"
+            + "";
+        assertThat(response.toString()).isEqualTo(expected);
+    }
+
+}
--- app/src/test/java/nextstep/jwp/model/HttpResponseTest.java
+++ app/src/test/java/nextstep/jwp/model/HttpResponseTest.java
@@ -3,19 +3,30 @@ package nextstep.jwp.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class HttpResponseTest {
 
     @Test
-    public void createResponse() {
-        HttpResponse response= new HttpResponse("html", "HTTP/1.1", 200, "nextstep".getBytes());
+    public void create_ok_response() {
+        HttpResponse response = HttpResponse.ok("html", "HTTP/1.1", "nextstep".getBytes());
         String expected = "HTTP/1.1 200 OK \r\n"
             + "Content-Type: text/html;charset=utf-8 \r\n"
             + "Content-Length: 8 \r\n"
             + "\r\n"
             + "nextstep";
+        assertThat(response.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void create_redirect_response() {
+        HttpResponse response = HttpResponse.redirect("static/index.html", "HTTP/1.1", 302);
+        String expected = "HTTP/1.1 302 Found \r\n"
+            + "Location: static/index.html \r\n"
+            + "\r\n"
+            + "";
         assertThat(response.toString()).isEqualTo(expected);
     }
 
